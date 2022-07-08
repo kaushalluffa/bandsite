e57f0362b18b3c8aa9043b2c57c75b21249cf72f
kaushalluffa <webdevelopment852@gmail.com>
2022-07-07 23:12:32 -0400

    date is now dynamic with use of momentjs

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -2,19 +2,19 @@
 const comments = [
   {
     name: "Connor Walton",
-    date: "02/17/2021",
+    date: moment("20210217", "YYYYMMDD").fromNow(),
     comment:
       "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
   },
   {
     name: "Emilie Beach",
-    date: "01/09/2021",
+    date: moment("20210901", "YYYYMMDD").fromNow(),
     comment:
       "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
   },
   {
     name: "Miles Acosta",
-    date: "12/20/2021",
+    date: moment("20211221", "YYYYMMDD").fromNow(),
     comment:
       "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
   },
@@ -55,7 +55,7 @@ function addComment(e) {
 
   const inputName = document.getElementById("name");
   const inputComment = document.getElementById("comment");
-  const newDate = moment().format("L");
+  const newDate = moment().fromNow();
   //if check to see if the values are empty so empty comments can be avoided
   if (inputName.value === "" && inputComment.value === "") {
     alert("Please enter name and comment");
